6224c47a3fad7215c17be35b6b92adcaafea93d6
Kim Seungha <seungha.me@gmail.com>
2020-08-17 16:55:57 +0900

    Initial Commit

---
+++ gomoku-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gomoku-ios
+//
+//  Created by Kim Seungha on 2020/08/17.
+//  Copyright © 2020 Kim Seungha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
